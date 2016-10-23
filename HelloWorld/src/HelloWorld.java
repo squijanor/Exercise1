3a1a89ba20f887ab3d547fde4b0319eeded3da5f
SQuijano <sergio.quijano@tum.de>
2016-10-23 18:45:36 +0200

    Change initial greeting text

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.io.*;
+
 /**
  * 
  */
@@ -6,13 +8,20 @@
  * @author Mohsen
  *
  */
-public class HelloWorld {
+public class HelloWorld{
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		System.out.println("Hello ASE2016 - how are you? :)");
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		HelloUser hu;
+		
+		System.out.println("Hello ASE2016 - how are you now? :)");
+		System.out.print("Please enter your name: ");
+		String name = br.readLine();
+		hu = new HelloUser(name);
+		hu.greetUser();
 	}
 
 }
